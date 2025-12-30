155933bee6f4d4cc5642ef6860e6128b8f95ed31
02anmol <tyagianmol839@gmail.com>
2025-12-30 15:12:45 +0530

    updation till here

--- internal/handlers/room.go
+++ internal/handlers/room.go
@@ -20,8 +20,19 @@ func Room(c *fiber.Ctx) error{
 		c.Status(400)
 		return nil
 	}
-
+	ws := "ws"
+	if os.Getenv("ENVIRONMENT") == "PRODUCTION"{
+		ws = "wss"
+	}
 	uuid, suuid, _ := createOrGetRoom(uuid)
+	return c.Render("peer", fiber.Map{
+		"RoomWebSocketAddr":fmt.Sprintf("%s://%s/room/websocket/%s", ws, c.Hostname(), uuid),
+		"RoomLink":fmt.Sprintf("%s://%s/room/%s", c.Protocol(), c.Hostname(), suuid),
+		"ChatWebSocketAddr":fmt.Sprintf("%s://%s/chat/websocket/%s", ws, c.Hostname(), uuid),
+		"StreamLink":fmt.Sprintf("%s://%s/stream/%s", c.Protocol(), c.Hostname(), suuid),
+		"ViewerWebSocketAddr":fmt.Sprintf("%s://%s/room/viewer/websocket/%s", ws, c.Hostname(), uuid),
+		"Type": "room",
+	},	"layouts/main")
 }
 
 func RoomWebSocket(c *websocket.Conn) {
@@ -38,7 +49,17 @@ func createOrGetRoom(uuid string)(string, string, *w.Room){
 }
 
 func RoomViewerWebSocket(c *websocket.Conn){
+	uuid := c.Pareams("uuid")
+	if uuid==""{
+		return
+	}
 
+	w.RoomsLock.Lock()
+	if peer, ok := w.Rooms[uuid]; ok{
+		roomViewerConn(c, peer.Peers)
+		return
+	}
+	w.RoomsLock.Unlock()
 }
 
 func roomViewerConn(c *websocket.Conn, p *w.Peers){
